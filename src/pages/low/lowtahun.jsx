04a5c9fcb457e90d308bc731830b8db1073c489d
young <youngscientist.iysa@gmail.com>
2024-08-19 16:08:05 +0700

    list winner

--- src/pages/low/lowtahun.jsx
+++ src/pages/low/lowtahun.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Navcomps from '../../components/Navcomps';
+import Navcomps from "../../components/Navcomps";
 import Footercomps from "../../components/Footercomps";
-
 
 const Lowtahun = () => {
   return (
@@ -13,23 +12,32 @@ const Lowtahun = () => {
             <div className="faq-title text-center pb-3">
               <h2>List of Winners</h2>
               <div className="">
-                <h3>Select by the year of the event you are participating in</h3>
+                <h3>
+                  Select by the year of the event you are participating in
+                </h3>
                 <a
-                  href="/kategori23"
+                  href="https://drive.google.com/file/d/1K1TwudQXrPCUJBC5Xtl0Ehqq8_V14gIN/view?usp=drive_link"
+                  target="_blank"
+                  rel="noreferrer"
                   className="btn btn--primary m-1"
                 >
+                  2024{" "}
+                </a>
+                <a href="/kategori23" className="btn btn--primary m-1">
                   2023{" "}
                 </a>
                 <a
                   href="https://drive.google.com/file/d/1SBCFBTN0npsohL1QJ33mPplpx1nWB8bB/view?usp=sharing"
-                  target="_blank" rel="noreferrer"
+                  target="_blank"
+                  rel="noreferrer"
                   className="btn btn--primary m-1"
                 >
                   2022{" "}
                 </a>
                 <a
                   href="https://drive.google.com/file/d/1mhA7cVX_6g0Ay4hNUXdU_yoTazb5C8dz/view?usp=sharing"
-                  target="_blank" rel="noreferrer"
+                  target="_blank"
+                  rel="noreferrer"
                   className="btn btn--primary m-1"
                 >
                   2021{" "}
